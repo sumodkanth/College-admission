5a37b6cdd3ca99f895f9f485eb3a76d088723227
sumodkanth <sumodkanthcs@gmail.com>
2024-04-16 23:42:06 +0530

    changes

--- AdminUI/admin.py
+++ AdminUI/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DepartmentDB, CourseDB, StudentDB, FacultyEnrollmentDB, JobsDB, InterviewStep, JobApplications, newsDB, newsDB2, placed_studdb, Marquee, JobStatus2,TrainingDB,CourseenrollmentDB,MultipleChoiceQuestion,Choice,TestResult, Payment,HostelRoom,BusBooking
+from .models import DepartmentDB, CourseDB, StudentDB, FacultyEnrollmentDB, JobsDB, placed_studdb,CourseenrollmentDB,MultipleChoiceQuestion,Choice,TestResult, Payment,HostelRoom,BusBooking
 
 # Register your models here.
 admin.site.register(DepartmentDB)
@@ -7,14 +7,12 @@ admin.site.register(CourseDB)
 admin.site.register(StudentDB)
 admin.site.register(FacultyEnrollmentDB)
 admin.site.register(JobsDB)
-admin.site.register(InterviewStep)
-admin.site.register(JobApplications)
-admin.site.register(newsDB)
-admin.site.register(newsDB2)
+
+
 admin.site.register(placed_studdb)
-admin.site.register(Marquee)
-admin.site.register(JobStatus2)
-admin.site.register(TrainingDB)
+
+
+
 admin.site.register(CourseenrollmentDB)
 admin.site.register(MultipleChoiceQuestion)
 admin.site.register(Choice)
--- FacultyUI/admin.py
+++ FacultyUI/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from .models import FacultyDB
+
 # Register your models here.
-admin.site.register(FacultyDB)
