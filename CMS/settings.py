d6b41aceb40788f486372f377c65209e7855e007
sumodkanth <sumodkanthcs@gmail.com>
2024-05-07 16:13:04 +0530

    mail update

--- CMS/settings.py
+++ CMS/settings.py
@@ -140,7 +140,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'rosadavisc@gmail.com'
-EMAIL_HOST_PASSWORD = 'ojln gfti yldm jraw'
+EMAIL_HOST_PASSWORD = 'looy mpsj gxte rbpj'
 # EMAIL_DEBUG = True
 
 # EMAIL_USE_SSL = True
